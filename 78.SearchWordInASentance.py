0171540a18572a4d90676737f8efd89a2004d213
Ravi <160601540+ravi-sidharth@users.noreply.github.com>
2024-09-23 05:32:40 +0000

    update

--- 78.SearchWordInASentance.py
+++ 78.SearchWordInASentance.py
@@ -7,13 +7,10 @@ for i in range(len(S)):
     if S[i] == W[count]:
         count+=1
         print(count)
-        print(len(W))
-    elif count == len(W):
-        print("Yes")
-        break
-
+        if count == len(W):
+            print("Yes")
+            quit()
     else:
         count =0
 
-else:
-    print("No")
+print("No")
